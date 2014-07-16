9f41e3d8702bdcf47d358ad7f7ea11150adcf2b1
rays <rays@raysdeMac-mini.local>
2014-07-16 16:43:52 +0800

    private del

--- application/models/letters.php
+++ application/models/letters.php
@@ -14,43 +14,36 @@
 		}
 
 		public function add($receiver_id, $text){
+
+			// 信
 			$sender_id = $this->auth->get_ID();
-			$query = $this->db->get_where('letters', array('sender_id'=>$sender_id, 'receiver_id'=>$receiver_id));
-			$letter = array();
+			$letter_s = array();
+			$letter_r = array();
 			$msg = array(
 				'author_id' => $sender_id,
 				'text' => $text,
 				'created' => date('y-m-d H:i:s',time()),
 			);
+
 			// 发信人列表
-			// 聊天列表不为空
+			$query = $this->db->get_where('letters', array('sender_id'=>$sender_id, 'receiver_id'=>$receiver_id));
 			if($query->num_rows() > 0 ){
 				$query = $this->db->select('content')->get_where('letters', array('sender_id'=>$sender_id, 'receiver_id'=>$receiver_id))->row_array();
-				$letter = unserialize($query['content']);
-				array_push($letter, $msg);
+				$letter_s = unserialize($query['content']);
+				array_push($letter_s, $msg);
 				$data = array(
-					'content' => serialize($letter),
+					'content' => serialize($letter_s),
 					'modified' => date('y-m-d H:i:s',time()),
 				);
 
 				// 更新发送者的列表
 				$this->db->where(array('sender_id'=>$sender_id, 'receiver_id'=>$receiver_id));
 				$query = $this->db->update('letters', $data);
-
-				// 更新接受者的列表
-				$query = $this->db->select('unread')->get_where('letters', array('sender_id'=>$receiver_id, 'receiver_id'=>$sender_id))->row_array();
-
-				$this->db->where(array('sender_id'=>$receiver_id, 'receiver_id'=>$sender_id));
-				$data['sender_id'] = $receiver_id;
-				$data['receiver_id'] = $sender_id;
-				$data['unread'] = ++$query['unread'];
-				$query = $this->db->update('letters', $data);
 			} 
-			// 聊天列表不为空
 			else {
-				array_push($letter, $msg);
+				array_push($letter_s, $msg);
 				$data = array(
-					'content' => serialize($letter),
+					'content' => serialize($letter_s),
 					'unread' => 0,
 					'sender_id' => $sender_id,
 					'receiver_id' => $receiver_id,
@@ -58,9 +51,34 @@
 					'modified' => date('y-m-d H:i:s',time()),
 				);
 				$query = $this->db->insert('letters', $data);
-				$data['sender_id'] = $receiver_id;
-				$data['receiver_id'] = $sender_id;
-				$data['unread'] = 1;
+			}
+
+			// 收件人列表
+			$query = $this->db->get_where('letters', array('sender_id'=>$receiver_id, 'receiver_id'=>$sender_id));
+			if($query->num_rows() > 0 ){
+				$query = $this->db->select('content')->get_where('letters', array('sender_id'=>$receiver_id, 'receiver_id'=>$sender_id))->row_array();
+				$letter_r = unserialize($query['content']);
+				array_push($letter_r, $msg);
+				$query = $this->db->select('unread')->get_where('letters', array('sender_id'=>$receiver_id, 'receiver_id'=>$sender_id))->row_array();
+				$this->db->where(array('sender_id'=>$receiver_id, 'receiver_id'=>$sender_id));
+				$data = array(
+					'content' => serialize($letter_r),
+					'modified' => date('y-m-d H:i:s',time()),
+					'sender_id' => $receiver_id,
+					'receiver_id' => $sender_id,
+					'unread' => ++$query['unread'],
+				);
+				$query = $this->db->update('letters', $data);
+			} else {
+				array_push($letter_r, $msg);
+				$data = array(
+					'content' => serialize($letter_r),
+					'unread' => 1,
+					'sender_id' => $receiver_id,
+					'receiver_id' => $sender_id,
+					'created' => date('y-m-d H:i:s',time()),
+					'modified' => date('y-m-d H:i:s',time()),
+				);
 				$query = $this->db->insert('letters', $data);
 			}
 		}
@@ -83,8 +101,6 @@
 			$query = $this->db->get_where('letters', array('sender_id'=>$sender_id, 'receiver_id'=>$receiver_id));
 			if($query->num_rows() > 0 ){
 				$query = $this->db->delete('letters', array('sender_id'=>$sender_id, 'receiver_id'=>$receiver_id));
-			} else {
-				return fasle;
 			}
 		}
 
